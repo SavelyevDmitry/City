82c317615d203c94605daf7c29b47f5deae465ce
Savelyev Dmitry <savelyev.ds@yandex.ru>
2020-07-12 16:34:03 +0300

    page service-info cardiology 12.07

--- src/js/pageService.js
+++ src/js/pageService.js
@@ -1,24 +1,36 @@
 $(document).ready(function () {
     const tabletime = document.querySelectorAll(".timetable");
     const tabletimeDoctor = document.querySelectorAll(".timetable__doctor");
+    const tabletimeButton = document.querySelector("#tabletime-btn");
     
     screen_check();
     $(window).on('resize', function(){
         screen_check();
     });
+
+    tabletimeButton.addEventListener("click", function() {
+        tabletime.forEach( function(item, index){
+            item.classList.remove("none")
+            tabletimeDoctor[index].classList.remove("none");
+            tabletimeButton.classList.add("none");
+        })
+    })
    
     function screen_check(){
         tabletime.forEach( function(item, index){
-            console.log(item);
-            console.log(index);
-            console.log($(window).width());
-            if ( $(window).width() <= 768 && index > 0 ) { 
+            if ( $(window).width() < 768 && index > 0 ) { 
                 item.classList.add("none");
                 tabletimeDoctor[index].classList.add("none");
-            } else {
+                tabletimeButton.classList.remove("none");
+            } else if (index > 1) {
+                item.classList.add("none");
+                tabletimeDoctor[index].classList.add("none");
+                tabletimeButton.classList.remove("none"); 
+            } else{
                 item.classList.remove("none")
                 tabletimeDoctor[index].classList.remove("none");
-            };
+                tabletimeButton.classList.remove("none");
+            }
         })
 
     }
